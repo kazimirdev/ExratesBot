500bb4cc90e917c56b23c69c4ef1e779c4295566
Greenboyisyourdream <pankazimierzwlesie@protonmail.com>
2022-04-16 15:33:42 +0200

    database middleware has been created

---
+++ bot/tgbot/middlewares/db.py
@@ -0,0 +1,10 @@
+from aiogram.dispatcher.middlewares import LifetimeControllerMiddleware
+
+
+class DbMiddleware(LifetimeControllerMiddleware):
+    skip_patterns = ["error", "update"]
+
+    async def pre_process(self, obj, data, *args):
+        db_session = obj.bot.get('db')
+        # Pass data from table to handler 
+        # data['some_model'] = await Model.get()
