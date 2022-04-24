f8b355cdf70eabff7d47d5579516ec3377547c15
Greenboyisyourdream <pankazimierzwlesie@protonmail.com>
2022-04-24 17:26:45 +0200

    parser has been made

---
+++ bot/tgbot/misc/specific_cryptoparse.py
@@ -0,0 +1,29 @@
+import aiohttp
+import lxml
+import re
+
+from bs4 import BeautifulSoup
+   
+    
+async def specific_crypro_parser(currency: str):
+    async with aiohttp.ClientSession() as session:
+        url = f"https://coinmarketcap.com/pl/currencies/{currency}/"
+        response = await session.get(url.lower().replace(" ","-"))
+        html = await response.text()
+        soup = BeautifulSoup(html, features="lxml")
+        if "something went wrong" in soup.text:
+            return None
+        else:
+            answer = {"result": True} 
+            table = soup.find('div', {'class': 'sc-16r8icm-0 nds9rn-0 dAxhCK'})
+            keys = [
+                    re.sub("<[^>]*>", 
+                    " ",
+                    str(t)).strip() for t in table.findAll("th")]
+            values = [
+                    re.sub("<[^>]*>",
+                    " ",
+                    str(t)).strip() for t in table.findAll("td")]
+            table = [[keys[i], values[i]] for i in range(len(keys))]
+            return table 
+
